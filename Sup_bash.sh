1d2a518126f3efd57efc0bb2f36a4b99ceb403ff
jasonmnagata <46951605+jasonmnagata@users.noreply.github.com>
2019-03-19 09:31:39 -0700

    Delete Sup_bash.sh

--- Sup_bash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=WeightLoss_sup_030819
-#SBATCH --mail-type=END,FAIL # notifications for job done & fail
-#SBATCH --mail-user=braudt@unc.edu  # email address to send job notification to 
-
-stata15-se -b do AH_WeightLossBehav_SupplementalAnalysis_030819.do 
